1686b4bde0f9e683a7d5f24e86dddfbbfc865c52
hparvez307 <hparvez307@gmail.com>
2023-05-02 00:04:42 +0600

    setup

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const chef = require('./data/chef.json');
 const app = express();
 const port = 5000;
+
 app.use(cors());
 
 app.get('/', (req,res)=> {
-    res.send('server is working properly');
+    res.send(chef);
 })
 
+app.get('/:id', (req,res)=> {
 
+    const id = req.params.id;
+    const selectedChef = chef.find( c => c.id === id);
 
-
-
-
+    res.send(selectedChef);
+})
 
 app.listen(port, ()=>{
     console.log('server is running');
