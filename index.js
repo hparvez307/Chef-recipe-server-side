a8b421a250638677cad208f3f77758d027c897f0
hparvez307 <hparvez307@gmail.com>
2023-05-01 22:56:33 +0600

    json

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const port = 5000;
 app.use(cors());
+
+app.get('/', (req,res)=> {
+    res.send('server is working properly');
+})
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log('server is running');
+})
